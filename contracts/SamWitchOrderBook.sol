2a35fd9450514d402f2a75489014730cbe05ea79
0xSamWitch <julian.assange12@outlook.com>
2024-01-26 16:59:34 +0000

    Remove MessageHashUtils unused import

--- contracts/SamWitchOrderBook.sol
+++ contracts/SamWitchOrderBook.sol
@@ -10,8 +10,6 @@ import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IERC2981} from "@openzeppelin/contracts/interfaces/IERC2981.sol";
 
-import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
-
 import {BokkyPooBahsRedBlackTreeLibrary} from "./BokkyPooBahsRedBlackTreeLibrary.sol";
 
 import {IBrushToken} from "./interfaces/IBrushToken.sol";
