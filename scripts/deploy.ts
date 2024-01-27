230f359366885df2de6cf6638d1c4bfd279a1930
0xSamWitch <julian.assange12@outlook.com>
2024-01-27 10:09:35 +0000

    The deployment script should corretly verify swob contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,14 +6,15 @@ import {SamWitchOrderBook} from "../typechain-types";
 // Deploy everything
 async function main() {
   const [owner] = await ethers.getSigners();
-  console.log(`Deploying contracts with the account: ${owner.address} on chain id: ${hre.network.config.chainId}`);
-
-  const {shouldVerify} = await networkConstants(hre);
+  console.log(
+    `Deploying contracts with the account: ${owner.address} on chain id: ${(await ethers.provider.getNetwork()).chainId}`,
+  );
 
   const brush = "0x85dec8c4B2680793661bCA91a8F129607571863d";
-  const dev = "0x3b99636439FBA6314C0F52D35FEd2fF442191407";
-  let estforItems = "0x4b9c90ebb1fa98d9724db46c4689994b46706f5a";
-  if (hre.network.config.chainId == 31337) {
+  const dev = "0x045eF160107eD663D10c5a31c7D2EC5527eea1D0";
+  // estforItemsLive = 0x4b9c90ebb1fa98d9724db46c4689994b46706f5a
+  let estforItems = "0x1dae89b469d15b0ded980007dfdc8e68c363203d";
+  if ((await ethers.provider.getNetwork()).chainId == 31337n) {
     const erc1155 = await ethers.deployContract("MockERC1155", [dev]);
     estforItems = await erc1155.getAddress();
   }
@@ -25,10 +26,12 @@ async function main() {
     kind: "uups",
   })) as unknown as SamWitchOrderBook;
   await swob.waitForDeployment();
+  console.log("Deployed SamWitchOrderBook to:", await swob.getAddress());
 
+  const {shouldVerify} = await networkConstants(hre);
   if (shouldVerify) {
     try {
-      const addresses: string[] = [];
+      const addresses: string[] = [await swob.getAddress()];
       console.log("Verifying contracts...");
       await verifyContracts(addresses);
     } catch (e) {
