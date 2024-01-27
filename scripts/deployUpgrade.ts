ee14aca1970d7658eaffc75178f9c80200eadbfb
0xSamWitch <julian.assange12@outlook.com>
2024-01-27 10:09:49 +0000

    Add upgrade script

---
+++ scripts/deployUpgrade.ts
@@ -0,0 +1,38 @@
+import hre, {ethers, upgrades} from "hardhat";
+import {verifyContracts} from "./helpers";
+import {SamWitchOrderBook} from "../typechain-types";
+import {networkConstants} from "../constants/network_constants";
+
+// Upgrade order book
+async function main() {
+  const [owner] = await ethers.getSigners();
+  console.log(
+    `Upgrading contracts with the account: ${owner.address} on chain id: ${(await ethers.provider.getNetwork()).chainId}`,
+  );
+
+  const timeout = 600 * 1000; // 10 minutes
+  const swobAddress = "0xE21b5eFf5d645821aC23C51214432cdA8D0446f6";
+
+  const SamWitchOrderBook = await ethers.getContractFactory("SamWitchOrderBook");
+  const swob = (await upgrades.upgradeProxy(swobAddress, SamWitchOrderBook, {
+    kind: "uups",
+    timeout,
+  })) as unknown as SamWitchOrderBook;
+  await swob.waitForDeployment();
+
+  const {shouldVerify} = await networkConstants(hre);
+  if (shouldVerify) {
+    try {
+      const addresses: string[] = [await swob.getAddress()];
+      console.log("Verifying contracts...");
+      await verifyContracts(addresses);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
