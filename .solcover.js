1d30d552dc99ddc88e5b03c3ae59aeb39c88d9e7
Justin Silver <jsilver@doublesharp.com>
2024-02-27 10:03:15 -0800

    upgrade solidity coverage
    https://github.com/sc-forks/solidity-coverage/issues/863

--- .solcover.js
+++ .solcover.js
@@ -1,25 +1,14 @@
 module.exports = {
   configureYulOptimizer: true,
   solcOptimizerDetails: {
+    peephole: false,
+    inliner: false,
+    jumpdestRemover: false,
+    orderLiterals: true, // <-- TRUE! Stack too deep when false
+    deduplicate: false,
+    cse: false,
+    constantOptimizer: false,
     yul: true,
-    yulDetails: {
-      optimizerSteps:
-        "dhfoDgvlfnTUtnIf" + // None of these can make stack problems worse
-        "[" +
-        "xa[r]EsLM" + // Turn into SSA and simplify
-        "CTUtTOntnfDIl" + // Perform structural simplification
-        "Ll" + // Simplify again
-        "Vl [j]" + // Reverse SSA
-        // should have good "compilability" property here.
-
-        "Tpel" + // Run functional expression inliner
-        "xa[rl]" + // Prune a bit more in SSA
-        "xa[r]L" + // Turn into SSA again and simplify
-        "gvf" + // Run full inliner
-        "CTUa[r]LSsTFOtfDna[r]Il" + // SSA plus simplify
-        "]" +
-        "jml[jl] VTOl jml : fDnTO",
-    },
   },
   skipFiles: ["BokkyPooBahsRedBlackTreeLibrary.sol", "test/"],
 };
