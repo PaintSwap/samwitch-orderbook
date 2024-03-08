52407ec1e8923b161b86abc6bcb3ab8da1c614a5
Justin Silver <jsilver@doublesharp.com>
2024-03-07 21:31:08 -0800

    fix coverage
    https://github.com/sc-forks/solidity-coverage/issues/870

--- .solcover.js
+++ .solcover.js
@@ -1,14 +1,3 @@
 module.exports = {
-  configureYulOptimizer: true,
-  solcOptimizerDetails: {
-    peephole: false,
-    inliner: false,
-    jumpdestRemover: false,
-    orderLiterals: true, // <-- TRUE! Stack too deep when false
-    deduplicate: false,
-    cse: false,
-    constantOptimizer: false,
-    yul: true,
-  },
   skipFiles: ["BokkyPooBahsRedBlackTreeLibrary.sol", "test/"],
 };
