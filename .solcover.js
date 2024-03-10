d9237826ec50b2351ff9aff13e54a6918b7d67b3
0xSamWitch <julian.assange12@outlook.com>
2024-03-10 09:13:54 +0000

    Revert "fix coverage"
    This reverts commit 52407ec1e8923b161b86abc6bcb3ab8da1c614a5.

--- .solcover.js
+++ .solcover.js
@@ -1,3 +1,14 @@
 module.exports = {
+  configureYulOptimizer: true,
+  solcOptimizerDetails: {
+    peephole: false,
+    inliner: false,
+    jumpdestRemover: false,
+    orderLiterals: true, // <-- TRUE! Stack too deep when false
+    deduplicate: false,
+    cse: false,
+    constantOptimizer: false,
+    yul: true,
+  },
   skipFiles: ["BokkyPooBahsRedBlackTreeLibrary.sol", "test/"],
 };
